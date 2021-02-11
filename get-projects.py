307f9fbe00dad5cbca2e711cfcc4c338b6230d16
William Jackson <william@subtlecoolness.com>
2021-02-11 15:09:11 -0600

    Update get-projects to write to database instead of csv file

--- get-projects.py
+++ get-projects.py
@@ -1,5 +1,5 @@
 import apscheduler.schedulers.blocking
-import csv
+import fort
 import logging
 import os
 import requests
@@ -9,63 +9,97 @@ from typing import Dict
 
 log = logging.getLogger('rfpio_api.get_projects')
 
+class Database(fort.PostgresDatabase):
+    def add_record(self, params):
+        sql = '''
+            insert into rfpio_projects (
+                project_id, project_name, client_name, status, project_type, stage, stage_comments,
+                sf_opportunity_stage, project_value, created_date, due_date, completed_date, last_updated_date,
+                project_owner, additional_primary_contacts, num_of_people, requester_email, signed_nda, region,
+                sf_account_id, sf_opportunity_id, software_location, test_project_field, education_services_needed,
+                list_of_products, ips_or_services_needed, num_sections, num_questions, num_not_answered,
+                num_answer_library_used, num_manual, archived_date, project_owner_name, internal_id, company_id
+            ) values (
+                %(project_id)s, %(project_name)s, %(client_name)s, %(status)s, %(project_type)s, %(stage)s,
+                %(stage_comments)s, %(sf_opportunity_stage)s, %(project_value)s, %(created_date)s, %(due_date)s,
+                %(completed_date)s, %(last_updated_date)s, %(project_owner)s, %(additional_primary_contacts)s,
+                %(num_of_people)s, %(requester_email)s, %(signed_nda)s, %(region)s, %(sf_account_id)s,
+                %(sf_opportunity_id)s, %(software_location)s, %(test_project_field)s, %(education_services_needed)s,
+                %(list_of_products)s, %(ips_or_services_needed)s, %(num_sections)s, %(num_questions)s,
+                %(num_not_answered)s, %(num_answer_library_used)s, %(num_manual)s, %(archived_date)s,
+                %(project_owner_name)s, %(internal_id)s, %(company_id)s
+            ) on conflict (project_id) do update set
+                project_name = %(project_name)s, client_name = %(client_name)s, status = %(status)s,
+                project_type = %(project_type)s, stage = %(stage)s, stage_comments = %(stage_comments)s,
+                sf_opportunity_stage = %(sf_opportunity_stage)s, project_value = %(project_value)s,
+                created_date = %(created_date)s, due_date = %(due_date)s, completed_date = %(completed_date)s,
+                last_updated_date = %(last_updated_date)s, project_owner = %(project_owner)s,
+                additional_primary_contacts = %(additional_primary_contacts)s, num_of_people = %(num_of_people)s,
+                requester_email = %(requester_email)s, signed_nda = %(signed_nda)s, region = %(region)s,
+                sf_account_id = %(sf_account_id)s, sf_opportunity_id = %(sf_opportunity_id)s,
+                software_location = %(software_location)s, test_project_field = %(test_project_field)s,
+                education_services_needed = %(education_services_needed)s, list_of_products = %(list_of_products)s,
+                ips_or_services_needed = %(ips_or_services_needed)s, num_sections = %(num_sections)s,
+                num_questions = %(num_questions)s, num_not_answered = %(num_not_answered)s,
+                num_answer_library_used = %(num_answer_library_used)s, num_manual = %(num_manual)s,
+                archived_date = %(archived_date)s, project_owner_name = %(project_owner_name)s,
+                internal_id = %(internal_id)s, company_id = %(company_id)s
+        '''
+        self.u(sql, params)
+
 
 def save_projects(projects, user_list=None):
     if user_list is None:
         user_list = {}
-    csv_field_names = [
-        'project_id', 'project_name', 'client_name', 'status', 'project_type', 'stage', 'stage_comments',
-        'sf_opportunity_stage', 'project_value', 'created_date', 'due_date', 'completed_date', 'last_updated_date',
-        'archived_date', 'project_owner', 'project_owner_name', 'additional_primary_contacts', 'num_of_people',
-        'requester_email', 'signed_nda', 'region', 'sf_account_id', 'sf_opportunity_id', 'software_location',
-        'test_project_field', 'education_services_needed', 'list_of_products', 'ips_or_services_needed', 'num_sections',
-        'num_questions', 'num_not_answered', 'num_answer_library_used', 'num_manual', 'internal_id', 'company_id'
-    ]
-    with open(os.getenv('OUTPUT_FILE'), 'w', newline='') as f:
-        writer = csv.DictWriter(f, fieldnames=csv_field_names)
-        writer.writeheader()
-        for p in projects:
-            p_products = p.get('customFields', {}).get('listofproducts', [])
-            if not isinstance(p_products, list):
-                p_products = [p_products]
-            pd = {
-                'project_id': p.get('customFields', {}).get('project_id'),
-                'project_name': p.get('name'),
-                'client_name': p.get('issuerName'),
-                'status': p.get('status', '').replace('_', ' ').title(),
-                'project_type': p.get('rfpType'),
-                'stage': p.get('projectStage', {}).get('stage'),
-                'stage_comments': p.get('projectStage', {}).get('comments'),
-                'sf_opportunity_stage': ', '.join(p.get('category', [])),
-                'project_value': p.get('dealSize', 0),
-                'created_date': p.get('createdDate', '')[:10],
-                'due_date': p.get('dueDate', '')[:10],
-                'completed_date': p.get('respondedDate', '')[:10],
-                'last_updated_date': p.get('lastUpdateTs', '')[:10],
-                'archived_date': p.get('archivedDate', '')[:10],
-                'project_owner': p.get('ownedBy'),
-                'project_owner_name': user_list.get(p.get('ownedBy')),
-                'additional_primary_contacts': ', '.join(p.get('additionalContacts', [])),
-                'num_of_people': len(p.get('teamMembers', [])),
-                'requester_email': p.get('customFields', {}).get('useremailaddress'),
-                'signed_nda': ', '.join(p.get('customFields', {}).get('nda', [])),
-                'region': ', '.join(p.get('customFields', {}).get('regionprojectlevel', [])),
-                'sf_account_id': p.get('customFields', {}).get('accountid'),
-                'sf_opportunity_id': p.get('customFields', {}).get('opportunityid'),
-                'software_location': p.get('customFields', {}).get('locationofsoftware'),
-                'test_project_field': p.get('customFields', {}).get('test_project_field'),
-                'education_services_needed': p.get('customFields', {}).get('infatraining'),
-                'list_of_products': ', '.join(p_products),
-                'ips_or_services_needed': p.get('customFields', {}).get('services_needed'),
-                'num_sections': p.get('sectionCount', 0),
-                'num_questions': p.get('totalQuestionCount', 0),
-                'num_not_answered': p.get('totalQuestionCount', 0) - p.get('totalAnsweredCount', 0),
-                'num_answer_library_used': p.get('totalAnsweredUsingAL', 0),
-                'num_manual': p.get('totalAnsweredCount', 0) - p.get('totalAnsweredUsingAL', 0),
-                'internal_id': p.get('id'),
-                'company_id': p.get('companyId')
-            }
-            writer.writerow(pd)
+    db = Database(os.getenv('DB'), minconn=3)
+    for p in projects:
+        p_products = p.get('customFields', {}).get('listofproducts', [])
+        if not isinstance(p_products, list):
+            p_products = [p_products]
+        pd = {
+            'project_id': p.get('customFields', {}).get('project_id'),
+            'project_name': p.get('name'),
+            'client_name': p.get('issuerName'),
+            'status': p.get('status', '').replace('_', ' ').title(),
+            'project_type': p.get('rfpType'),
+            'stage': p.get('projectStage', {}).get('stage'),
+            'stage_comments': p.get('projectStage', {}).get('comments'),
+            'sf_opportunity_stage': ', '.join(p.get('category', [])),
+            'project_value': p.get('dealSize', 0),
+            'created_date': p.get('createdDate', '')[:10],
+            'due_date': p.get('dueDate', '')[:10],
+            'completed_date': p.get('respondedDate', '')[:10],
+            'last_updated_date': p.get('lastUpdateTs', '')[:10],
+            'archived_date': p.get('archivedDate', '')[:10],
+            'project_owner': p.get('ownedBy'),
+            'project_owner_name': user_list.get(p.get('ownedBy')),
+            'additional_primary_contacts': ', '.join(p.get('additionalContacts', [])),
+            'num_of_people': len(p.get('teamMembers', [])),
+            'requester_email': p.get('customFields', {}).get('useremailaddress'),
+            'signed_nda': ', '.join(p.get('customFields', {}).get('nda', [])),
+            'region': ', '.join(p.get('customFields', {}).get('regionprojectlevel', [])),
+            'sf_account_id': p.get('customFields', {}).get('accountid'),
+            'sf_opportunity_id': p.get('customFields', {}).get('opportunityid'),
+            'software_location': p.get('customFields', {}).get('locationofsoftware'),
+            'test_project_field': p.get('customFields', {}).get('test_project_field'),
+            'education_services_needed': p.get('customFields', {}).get('infatraining'),
+            'list_of_products': ', '.join(p_products),
+            'ips_or_services_needed': p.get('customFields', {}).get('services_needed'),
+            'num_sections': p.get('sectionCount', 0),
+            'num_questions': p.get('totalQuestionCount', 0),
+            'num_not_answered': p.get('totalQuestionCount', 0) - p.get('totalAnsweredCount', 0),
+            'num_answer_library_used': p.get('totalAnsweredUsingAL', 0),
+            'num_manual': p.get('totalAnsweredCount', 0) - p.get('totalAnsweredUsingAL', 0),
+            'internal_id': p.get('id'),
+            'company_id': p.get('companyId')
+        }
+        if pd.get('archived_date') == '':
+            pd.update({'archived_date': None})
+        if pd.get('completed_date') == '':
+            pd.update({'completed_date': None})
+        if pd.get('project_id') is None:
+            continue
+        db.add_record(pd)
 
 
 def get_projects(token):
